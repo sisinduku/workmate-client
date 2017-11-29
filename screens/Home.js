06f58542fc9afb8fe88b26d8da51d52c707a35bf
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-11-29 17:05:10 +0700

    modify home buttons to use react-native-elements

--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Button, Slider, Divider, Card } from 'react-native-elements';
 
 export default class Home extends Component {
   render() {
     const { navigate } = this.props.navigation; 
     return (
       <View style={styles.container}>
-        <Text>Home Screen</Text>
-        <TouchableOpacity onPress={() => navigate('CreateJobSeekerProfile')}>
-          <Text>Job Seeker</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigate('SearchJobSeekers')}>
-          <Text>Employer</Text>
-        </TouchableOpacity>
+        <Button
+          large
+          style={{ marginBottom: 16 }}
+          title='JOB SEEKER'
+          onPress={() => navigate('CreateJobSeekerProfile')}
+        />
+        <Button
+          large
+          style={{ marginBottom: 16 }}
+          title='EMPLOYER'
+          onPress={() => navigate('SearchJobSeekers')}
+        />
       </View>
     );
   }
@@ -22,7 +28,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
