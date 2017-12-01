e6591047d1520f2e399879e7b470f4a352851d28
kholes <cholis.elbazar@gmail.com>
2017-12-01 15:37:29 +0700

    clear camera

--- screens/CreateJobSeekerProfile.js
+++ screens/CreateJobSeekerProfile.js
@@ -8,7 +8,6 @@ import {
   Image, 
   Platform
 } from 'react-native';
-import Camera from 'react-native-camera'
 import { connect } from 'react-redux'
 import { 
   FormLabel, 
@@ -56,14 +55,6 @@ class CreateJobSeekerProfile extends Component {
             <FormLabel labelStyle={styles.label}>SKILLS</FormLabel>
             <FormInput onChangeText={(e) => this.saveProfile(e, 'skills')} inputStyle={styles.input} />
             <FormLabel labelStyle={styles.label}>SUMMARY</FormLabel>
-            <Camera
-          ref={(cam) => {
-            this.camera = cam;
-          }}
-          style={styles.preview}
-          aspect={Camera.constants.Aspect.fill}>
-          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
-        </Camera>
             <FormInput onChangeText={(e) => this.saveProfile(e, 'summary')} multiline={true} numberOfLines={1} inputStyle={styles.input} />
             <Button
               title='SUBMIT' 
